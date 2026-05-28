29c8d505c1d56ff4eafb99904a75dc4c50b8288f
vivek <codecrafter67@gmail.com>
2026-05-28 12:02:30 +0530

    add condition and its test case

--- 01_testing/unit/intermediate/src/index.ts
+++ 01_testing/unit/intermediate/src/index.ts
@@ -7,6 +7,12 @@ app.use(express.json());
 app.post("/sum", (req, res) => {
   const { a, b } = req.body;
 
+  if (a>100000 || b > 100000){
+    return res.status(422).json({
+      message:"Sorry we dont process big numbers"
+    })
+  }
+
   res.json({
     answer: a + b,
   });
--- 01_testing/unit/intermediate/src/tests/index.test.ts
+++ 01_testing/unit/intermediate/src/tests/index.test.ts
@@ -5,22 +5,30 @@ import app from '../index.js';
 
 describe('return sum of the numbers ', () => {
   test('Should return positive number if add two positive numbers', async () => {
-  const res = await request(app)
-    .post('/sum')
-    .send({ a: 5, b: 2 });
+    const res = await request(app)
+      .post('/sum')
+      .send({ a: 5, b: 2 });
 
-  expect(res.status).toBe(200);
-  expect(res.body.answer).toBe(7);
+    expect(res.status).toBe(200);
+    expect(res.body.answer).toBe(7);
   })
 
 
   test('Should return negative number if add two negative numbers', async () => {
-  const res = await request(app)
-    .post('/sum')
-    .send({ a: -5, b: -3 });
+    const res = await request(app)
+      .post('/sum')
+      .send({ a: -5, b: -3 });
 
-  expect(res.status).toBe(200);
-  expect(res.body.answer).toBe(-8);
-});
+    expect(res.status).toBe(200);
+    expect(res.body.answer).toBe(-8);
+  })
 
+  test('Should return message if number is too big', async () => {
+    const res = await request(app)
+      .post('/sum')
+      .send({ a: 1000000, b: -3 });
+
+    expect(res.status).toBe(422);
+    expect(res.body.message).toBe("Sorry we dont process big numbers");
+  })
 })
