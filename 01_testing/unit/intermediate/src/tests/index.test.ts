a935f7ca89f14265cb789cf9fe4aaa44b8ca065d
vivek <codecrafter67@gmail.com>
2026-05-28 11:47:22 +0530

    add supertest library for testing

--- 01_testing/unit/intermediate/src/tests/index.test.ts
+++ 01_testing/unit/intermediate/src/tests/index.test.ts
@@ -1,22 +1,26 @@
 import { describe, expect, test } from 'vitest'
-import axios from 'axios'
+import request from "supertest"
+import app from '../index.js';
 
 
 describe('return sum of the numbers ', () => {
-  test('Should return positive number if add two postive numbers', async() => {
-    const res = await axios.post("http://localhost:3000/sum",{
-      a:3,b:5
-    })
-    console.log(res)
-    expect(res.data.answer).toBe(8);
-    
+  test('Should return positive number if add two positive numbers', async () => {
+  const res = await request(app)
+    .post('/sum')
+    .send({ a: 5, b: 2 });
+
+  expect(res.status).toBe(200);
+  expect(res.body.answer).toBe(7);
   })
 
-  // test('Should return negative numbers if add two negative numbers', () => {
-    
-  // })
 
-  // test('returns 0  if add two zeros', () => {
-    
-  // })
+  test('Should return negative number if add two negative numbers', async () => {
+  const res = await request(app)
+    .post('/sum')
+    .send({ a: -5, b: -3 });
+
+  expect(res.status).toBe(200);
+  expect(res.body.answer).toBe(-8);
+});
+
 })
