87a25cf9ac3e759fdd737968eacb3fc5ed3a0a68
vivek <codecrafter67@gmail.com>
2026-06-01 11:30:11 +0530

    implement the calesthenics workouts backend

---
+++ 02_cicd/calesthenics-backend/data.py
@@ -0,0 +1,189 @@
+workouts = {
+    "push": {
+      "basic": {
+        "name": "Basic Push Workout",
+        "exercises": [
+          {
+            "name": "Wall Push Up",
+            "sets": 3,
+            "reps": "10-15"
+          },
+          {
+            "name": "Incline Push Up",
+            "sets": 3,
+            "reps": "8-12"
+          },
+          {
+            "name": "Knee Push Up",
+            "sets": 3,
+            "reps": "8-12"
+          }
+        ]
+      },
+      "intermediate": {
+        "name": "Intermediate Push Workout",
+        "exercises": [
+          {
+            "name": "Standard Push Up",
+            "sets": 4,
+            "reps": "10-15"
+          },
+          {
+            "name": "Diamond Push Up",
+            "sets": 3,
+            "reps": "8-12"
+          },
+          {
+            "name": "Decline Push Up",
+            "sets": 3,
+            "reps": "8-12"
+          }
+        ]
+      },
+      "advanced": {
+        "name": "Advanced Push Workout",
+        "exercises": [
+          {
+            "name": "Archer Push Up",
+            "sets": 4,
+            "reps": "6-10"
+          },
+          {
+            "name": "Pseudo Planche Push Up",
+            "sets": 4,
+            "reps": "5-8"
+          },
+          {
+            "name": "One Arm Push Up Progression",
+            "sets": 4,
+            "reps": "3-6"
+          }
+        ]
+      }
+    },
+    "pull": {
+      "basic": {
+        "name": "Basic Pull Workout",
+        "exercises": [
+          {
+            "name": "Scapula Pulls",
+            "sets": 3,
+            "reps": "8-12"
+          },
+          {
+            "name": "Australian Rows",
+            "sets": 3,
+            "reps": "8-12"
+          },
+          {
+            "name": "Band Assisted Pull Up",
+            "sets": 3,
+            "reps": "5-8"
+          }
+        ]
+      },
+      "intermediate": {
+        "name": "Intermediate Pull Workout",
+        "exercises": [
+          {
+            "name": "Pull Up",
+            "sets": 4,
+            "reps": "6-10"
+          },
+          {
+            "name": "Chin Up",
+            "sets": 3,
+            "reps": "8-12"
+          },
+          {
+            "name": "Wide Grip Australian Row",
+            "sets": 3,
+            "reps": "10-15"
+          }
+        ]
+      },
+      "advanced": {
+        "name": "Advanced Pull Workout",
+        "exercises": [
+          {
+            "name": "Archer Pull Up",
+            "sets": 4,
+            "reps": "4-8"
+          },
+          {
+            "name": "Typewriter Pull Up",
+            "sets": 4,
+            "reps": "4-8"
+          },
+          {
+            "name": "Muscle Up Progression",
+            "sets": 5,
+            "reps": "3-5"
+          }
+        ]
+      }
+    },
+    "legs": {
+      "basic": {
+        "name": "Basic Legs Workout",
+        "exercises": [
+          {
+            "name": "Bodyweight Squat",
+            "sets": 3,
+            "reps": "15-20"
+          },
+          {
+            "name": "Glute Bridge",
+            "sets": 3,
+            "reps": "12-15"
+          },
+          {
+            "name": "Calf Raise",
+            "sets": 3,
+            "reps": "20-25"
+          }
+        ]
+      },
+      "intermediate": {
+        "name": "Intermediate Legs Workout",
+        "exercises": [
+          {
+            "name": "Bulgarian Split Squat",
+            "sets": 4,
+            "reps": "8-12"
+          },
+          {
+            "name": "Jump Squat",
+            "sets": 3,
+            "reps": "10-15"
+          },
+          {
+            "name": "Walking Lunges",
+            "sets": 3,
+            "reps": "12 each leg"
+          }
+        ]
+      },
+      "advanced": {
+        "name": "Advanced Legs Workout",
+        "exercises": [
+          {
+            "name": "Pistol Squat",
+            "sets": 4,
+            "reps": "5-8"
+          },
+          {
+            "name": "Shrimp Squat",
+            "sets": 4,
+            "reps": "5-8"
+          },
+          {
+            "name": "Jumping Pistol Squat",
+            "sets": 3,
+            "reps": "3-5"
+          }
+        ]
+      }
+    }
+  
+}
---
+++ 02_cicd/calesthenics-backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from data import workouts
+
+app = FastAPI()
+
+
+@app.get("/workouts")
+def get_workout(type: str = "push", level: str = "basic"):
+
+    if type not in ["push", "pull", "legs"]:
+        return {
+            "message": "Please provide type from these",
+            type: ["push", "pull", "legs"],
+        }   
+    elif level not in ["basic", "intermediate", "advanced"]:
+        return {
+            "message": "Please provide level from these",
+            type: ["basic", "intermediate", "advanced"],
+        }
+    else:
+        return workouts[type][level]
