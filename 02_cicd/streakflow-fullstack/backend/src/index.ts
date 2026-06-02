dd12e0da85ed8fe5d8cb44ddd9386500baff12e6
vivek <codecrafter67@gmail.com>
2026-06-02 12:51:02 +0530

    allow ips

--- 02_cicd/streakflow-fullstack/backend/src/index.ts
+++ 02_cicd/streakflow-fullstack/backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 
-app.listen(env.PORT, () => {
+app.listen(env.PORT,"0.0.0.0.", () => {
   console.log(`Server is running on http://localhost:${env.PORT}`);
 });
 
