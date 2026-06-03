af800157edc8f184706a853728c26a4cd955901c
vivek <codecrafter67@gmail.com>
2026-06-03 12:26:12 +0530

    fix bugs

--- 02_cicd/streakflow-fullstack/backend/src/modules/auth/auth.routes.ts
+++ 02_cicd/streakflow-fullstack/backend/src/modules/auth/auth.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authenticate } from "../../middlewares/authenticate.js";
 import { validateRequest } from "../../middlewares/validate-request.js";
 import { login, logout, me, refresh, register } from "./auth.controller.js";
-import { loginSchema, logoutSchema, refreshSchema, registerSchema } from "./auth.schemas.js";
+import { loginSchema, logoutSchema, registerSchema } from "./auth.schemas.js";
 
 export const authRouter = Router();
 
