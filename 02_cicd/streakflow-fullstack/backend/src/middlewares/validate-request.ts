15a8926b4f9b6fd7a1e9a610ace5172af6d31f5f
vivek <codecrafter67@gmail.com>
2026-06-03 12:40:24 +0530

    add empty obj in validate req

--- 02_cicd/streakflow-fullstack/backend/src/middlewares/validate-request.ts
+++ 02_cicd/streakflow-fullstack/backend/src/middlewares/validate-request.ts
@@ -4,9 +4,9 @@ import type { ZodTypeAny } from "zod";
 export const validateRequest = (schema: ZodTypeAny) => {
   return (req: Request, _res: Response, next: NextFunction) => {
     schema.parse({
-      body: req.body,
-      params: req.params,
-      query: req.query,
+      body: req.body ?? {},
+      params: req.params ?? {},
+      query: req.query ?? {},
     });
     next();
   };
